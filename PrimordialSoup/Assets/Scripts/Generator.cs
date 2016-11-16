22433d590276f1f2a4a27c0f5378946f89363b1f
Nabil Babu <nabil.babu20@gmail.com>
2016-11-16 00:16:26 -0500

    Still trying to solve repeating pattern problem

--- PrimordialSoup/Assets/Scripts/Generator.cs
+++ PrimordialSoup/Assets/Scripts/Generator.cs
@@ -5,11 +5,8 @@ public class Generator : MonoBehaviour {
 
 	public GameObject tilePrefab;
 
-	public int minX = -11;
 	public int maxX = 11;
-	public int minY = -10;
 	public int maxY = 10; 
-
 	public int waterLevel = 50; 
 
 	PerlinNoise noise;
@@ -25,21 +22,23 @@ public class Generator : MonoBehaviour {
 		float height = tilePrefab.transform.lossyScale.y;
 
 
-		for (int i = minX; i < maxX; i++) {
-			for (int j = minY; j < maxY; j++) {
-				int zz = noise.getNoise (i - minX, j - minY, 100); //getNoise breaks down if i < 0, so we subtract the minX
+		for (int i = 0; i < maxX; i++) {
+			 
+			for (int j = 0; j < maxY; j++) {
+				//Debug.Log ("x index, y index "+xCount+" "+yCount);
+				int zz = noise.getNoise (i, j, 100); 
 				GameObject block = (GameObject) Instantiate (tilePrefab, new Vector2(i + width, j + height), tilePrefab.transform.rotation);
 				if (zz < waterLevel) {
 					float shade = 0;
-					shade = 1 - ((float) zz / (float) waterLevel);
+					shade = ((float) zz / (float) waterLevel);
 					block.GetComponent<TileChanger>().Color (0, 0, shade);	
 				} else {
 					float shade = 0;
-					shade = 1 - (((float) zz - (float)waterLevel)/ (float) waterLevel);
+					shade = (((float) zz - (float)waterLevel)/ (float) waterLevel);
 					block.GetComponent<TileChanger>().Color (0, shade, 0);	
-				}
-				 
+				} 
 			}
+			 
 		}
 	}
 }
