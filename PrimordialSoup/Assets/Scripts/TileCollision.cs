53edf1f856d359d5e1472c38873b72ce359503a7
Nabil Babu <nabil.babu20@gmail.com>
2016-11-24 23:32:28 -0500

    Environment can be destroyed by colliding into it

---
+++ PrimordialSoup/Assets/Scripts/TileCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileCollision : MonoBehaviour {
+
+	public GameObject blueTile;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			Instantiate (blueTile, new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y), blueTile.transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
